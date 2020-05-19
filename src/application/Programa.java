c8ddc8f346c4bb7e1fddfb75a0e3e926021b2f8e
Fernando Braga Alves <f.braga.alves1@gmail.com>
2020-05-19 12:05:48 -0300

    Primeira classe adicionada

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,4 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Programa {
+
+    public static void main(String[] args) {
+        Position position  = new Position(2,3);
+
+        System.out.println(position.toString());
+    }
 }
